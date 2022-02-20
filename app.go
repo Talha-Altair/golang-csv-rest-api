f7c7e0d75b67558afbb8d60481acd160398a6fdf
Talha-Altair <cooltalha27@gmail.com>
2022-02-20 18:49:27 +0530

    Updates for 2022-02-20 18:49:27

--- app.go
+++ app.go
@@ -1,50 +1,28 @@
 package main
 
 import (
-    "encoding/csv"
-    "fmt"
-    "os"
-)
 
-type empData struct {
-    id string
-    state string
-    city string
-}
+	"fmt"
 
-func read() {
+	"github.com/gin-gonic/gin"
 
-    csvFile, err := os.Open("./data.csv")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer csvFile.Close()
-    
-    csvLines, err := csv.NewReader(csvFile).ReadAll()
-
-    if err != nil {
-        fmt.Println(err)
-    }
-
-	data := {}
-
-    for _, line := range csvLines {
-
-        emp := empData{
-            id: line[0],
-            state: line[1],
-			city: line[2],
-        }
-
-		data.append(emp)
-
-        fmt.Println(emp.state)
-    }
-
-	fmt.Println(data)
-}
+	// "os"
+	
+	)
 
 func main() {
-	read()
+
+	r := gin.Default()
+
+	fmt.Println("Starting server")
+
+	r.GET("/ping", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{
+			"ping" : "pong",
+		})
+	})
+
+	r.Run("0.0.0.0:9000")
+
 }
---
+++ csv_data.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+    "encoding/csv"
+    "fmt"
+    "os"
+)
+
+type cityData struct {
+    id string
+    state string
+    city string
+}
+
+func read() []cityData {
+
+    csvFile, err := os.Open("./data.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer csvFile.Close()
+    
+    csvLines, err := csv.NewReader(csvFile).ReadAll()
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+	data := []cityData{}
+
+    for _, line := range csvLines {
+
+        city := cityData{
+            id: line[0],
+            state: line[1],
+			city: line[2],
+        }
+
+		if city.id == "id" {
+			continue
+		}
+
+		data = append(data, city)
+    }
+
+	return data
+}
+
+func main() {
+
+
+	data := read()
+
+	fmt.Println(data[0].id)
+
+}
