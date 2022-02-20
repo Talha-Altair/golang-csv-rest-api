277025cf682f94c2beb5a5432513930026f046d7
Talha-Altair <cooltalha27@gmail.com>
2022-02-20 19:14:03 +0530

    Updates for 2022-02-20 19:14:03

--- app.go
+++ app.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-
-	// "csv_data"
 	
 	)
 
@@ -23,6 +21,41 @@ func main() {
 		c.JSON(200, gin.H{
 			"ping" : "pong",
 		})
+	})
+
+	r.GET("/:city", func(c *gin.Context) {
+
+		city := c.Param("city")
+
+		fmt.Println("City:", city)
+
+		flag := false
+
+		for _, v := range data {
+
+			fmt.Println("City:", v.city)
+
+			if v.city == city {
+
+			flag = true
+
+			res := gin.H{
+				"City": v.city,
+				"State": v.state,
+				"id": v.id,
+			}
+
+			c.JSON(200, res)
+
+			}
+		}
+
+		if !flag {
+			
+			c.JSON(404, gin.H{
+				"error": "City not found",
+			})
+		}
 	})
 
 	r.Run("0.0.0.0:9000")
